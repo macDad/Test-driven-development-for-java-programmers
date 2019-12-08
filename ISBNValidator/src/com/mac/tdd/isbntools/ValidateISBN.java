8ba3fc00fffd5de76960ab5424afd1d119acfdb0
MadukaJ <jaya.maduka@gmail.com>
2019-12-08 21:15:29 +0800

    Besic TEst impl

---
+++ ISBNValidator/src/com/mac/tdd/isbntools/ValidateISBN.java
@@ -0,0 +1,22 @@
+package com.mac.tdd.isbntools;
+
+public class ValidateISBN {
+    public boolean checkISBN(String isbn) {
+
+        if (isbn.length() == 13) return true;
+        if (isbn.length() != 10) throw new NumberFormatException("ISBN number must be 10 digits long");
+        int total = 0;
+        for (int i = 0; i < 10; i++) {
+            if (!Character.isDigit(isbn.charAt(i))) {
+                if (i == 9 && isbn.charAt(i) == 'X') {
+                    total += 10;
+                } else
+                    throw new NumberFormatException("ISBN number can only contain numeric digits");
+            } else
+                total += Character.getNumericValue(isbn.charAt(i)) * (10 - i);
+
+        }
+        if (total % 11 == 0) return true;
+        return false;
+    }
+}
---
+++ ISBNValidator/test/com/mac/tdd/isbntools/ValidateISBNTest.java
@@ -0,0 +1,66 @@
+package com.mac.tdd.isbntools;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidateISBNTest {
+    @Test
+    public void checkAValid10DigitsISBN() {
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("0140449116");
+        assertTrue(result, "first value");
+        result = validator.checkISBN("0140177396");
+        assertTrue(result, "second value");
+
+    }
+
+    @Test
+    public void checkAValid13DigitsISBN() {
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("9780984782802");
+        assertTrue(result, "first value");
+        result = validator.checkISBN("9780262033848");
+        assertTrue(result, "second value");
+    }
+
+    @Test
+    public void TenDigitsISBNNumberEndingInAnXAreValid() {
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("098478280X");
+        assertTrue(result);
+    }
+
+    @Test
+    public void checkAnInvalid10DigitsISBN() {
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("0140449117");
+        assertFalse(result);
+    }
+    @Test
+    public void checkAnInvalid13DigitsISBN() {
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("9780984782804");
+        assertFalse(result);
+        fail();
+    }
+
+    //junit 4 @Test(expected = StringIndexOutOfBoundsException.class)
+    @Test
+    public void nineDigitISBNsAreNotAllowed() {
+        ValidateISBN validator = new ValidateISBN();
+        assertThrows(NumberFormatException.class,
+                () -> {
+                    validator.checkISBN("123456789");
+                });
+    }
+
+    @Test
+    public void nonNumericISBNAreNotAllowed() {
+        ValidateISBN validator = new ValidateISBN();
+        assertThrows(NumberFormatException.class,
+                () -> {
+                    validator.checkISBN("helloworld");
+                });
+    }
+}
